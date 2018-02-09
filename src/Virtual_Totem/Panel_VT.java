26e197f235e037d8cbd98acc482cdec62cd3ac45
yago.esh <yago.esh@gmail.com>
2018-02-09 14:25:16 +0100

    name of client
    Now when you take the totem, the other users can see your name in the totem you took.

--- src/Virtual_Totem/Panel_VT.java
+++ src/Virtual_Totem/Panel_VT.java
@@ -16,11 +16,13 @@ public class Panel_VT extends JPanel {
 	private JButton Lobo_bt;
 	private JButton Dragon_bt;
 	private boolean accion;
+	private String set_name;
 	
 	public Panel_VT(Client_VT cliente) {
 		
 		cliente.asociar(this);
 		accion=false;
+		set_name="";
 		setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
 		setForeground(Color.BLACK);
 		setBackground(Color.LIGHT_GRAY);
@@ -32,6 +34,7 @@ public class Panel_VT extends JPanel {
 				if (Lobo_bt.getText() == "Coger Lobo") {
 					accion=true;
 					cliente.enviar("coger_lobo");
+					cliente.enviar(System.getProperty("user.name"));
 				}
 				else {
 					accion=true;
@@ -48,6 +51,7 @@ public class Panel_VT extends JPanel {
 				if (Dragon_bt.getText() == "Coger Dragon") {
 					accion=true;
 					cliente.enviar("coger_dragon");
+					cliente.enviar(System.getProperty("user.name"));
 				}
 				else {
 					accion=true;
@@ -93,16 +97,30 @@ public class Panel_VT extends JPanel {
 					switch (texto){
 		        	case "coger_dragon":
 		        		Dragon_bt.setEnabled(false);
+		        		set_name="dragon";
 		        		break;
 		        	case "coger_lobo":
 		        		Lobo_bt.setEnabled(false);
+		        		set_name="lobo";
 		        		break;
 		        	case "soltar_dragon":
 		        		Dragon_bt.setEnabled(true);
+		        		Dragon_bt.setText("Coger Dragon");
 		        		break;
 		        	case "soltar_lobo":
 		        		Lobo_bt.setEnabled(true);
+		        		Lobo_bt.setText("Coger Lobo");
 		        		break;
+		        	default:
+		        		if (set_name == "dragon") {
+		        			Dragon_bt.setText("Dragon: " + texto);
+		        			set_name="";
+		        		}
+		        		else if (set_name == "lobo") {
+		        			Lobo_bt.setText("Lobo: " + texto);
+		        			set_name="";
+		        		}
+		        		
 		        	}
 				}
 			}
