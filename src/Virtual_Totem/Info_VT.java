df0a824819231e05b596005ed51c0819c21e2c80
yago.esh <yago.esh@gmail.com>
2018-02-22 16:36:01 +0100

    refactor class
    Refactored the class Info_VT and the code was cleaned

--- src/Virtual_Totem/Info_VT.java
+++ src/Virtual_Totem/Info_VT.java
@@ -18,153 +18,170 @@ import javax.swing.ImageIcon;
 
 public class Info_VT extends JDialog {
 
+	private Panel_VT panel;
+	private boolean unlock_dragon, unlock_wolf;
 	private static final long serialVersionUID = 1L;
 	private final JPanel contentPanel = new JPanel();
-	private JButton btnDesbloquearLobo;
-	private JButton btnDesbloquearDragon;
-	private boolean desb_dragon;
-	private boolean desb_lobo;
-
+	private JButton BT_unlock_dragon, BT_unlock_wolf, okButton, cancelButton;
+	
 	public Info_VT(Panel_VT panel) {
 		
-		this.setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
-		this.addWindowListener(new WindowAdapter() {
-	        @Override
-	        public void windowClosing(WindowEvent event) {
-	            hideIt();
-	        }
-	    });
-		desb_dragon=false;
-		desb_lobo=false;
-		this.setTitle("Info of Virtual Totem");
-		setBounds(810, 425, 451, 289);
-		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setBorder(null);
-		getContentPane().add(contentPanel, BorderLayout.CENTER);
-		contentPanel.setLayout(null);
-		contentPanel.setOpaque(false);
-		
+		this.panel=panel;
 		this.setVisible(false);
+		this.setResizable(false);
+		contentPanel.setLayout(null);
+		setBounds(810, 425, 451, 275);
+		this.setTitle("Info of Virtual Totem");
+		this.setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		
+		//------------------------------------TextFields---------------------------------------//
+		
 		JTextPane info_text = new JTextPane();
-		info_text.setForeground(Color.BLACK);
-		info_text.setEditable(false);
 		info_text.setOpaque(false);
+		info_text.setEditable(false);
+		info_text.setForeground(Color.BLACK);
 		info_text.setFont(new Font("Tahoma", Font.BOLD, 13));
 		info_text.setBackground(UIManager.getColor("CheckBox.background"));
 		info_text.setText("A continuaci\u00F3n va a proceder a desbloquear uno de los totem.\r\nEsta acci\u00F3n solo debe realizarse en los siguientes casos y solo tras haber corroborado la acci\u00F3n con el resto del equipo:\r\n\r\n\u2022 El programa ha bloqueado de forma incorrecta un totem.\r\n\u2022 Un usuario ha bloqueado de forma indefinida un totem, no est\u00E1 siendo utilizado y no puede liberarlo de forma manual.");
 		info_text.setBounds(12, 13, 408, 142);
 		contentPanel.add(info_text);
-		{
-			btnDesbloquearLobo = new JButton("Desbloquear Lobo");
-			btnDesbloquearLobo.setForeground(Color.BLACK);
-			btnDesbloquearLobo.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent arg0) {
-					if(!desb_lobo) {
-						btnDesbloquearLobo.setForeground(Color.RED);
-						desb_lobo=true;
-					}
-					else{
-						btnDesbloquearLobo.setForeground(Color.BLACK);
-						desb_lobo=false;
-					}
-				}
-			});
-			btnDesbloquearLobo.setBounds(35, 168, 150, 25);
-			contentPanel.add(btnDesbloquearLobo);
-		}
-		{
-			btnDesbloquearDragon = new JButton("Desbloquear Drag\u00F3n");
-			btnDesbloquearDragon.setForeground(Color.BLACK);
-			btnDesbloquearDragon.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent e) {
-					if(!desb_dragon) {
-						btnDesbloquearDragon.setForeground(Color.RED);
-						desb_dragon=true;
-					}
-					else {
-						btnDesbloquearDragon.setForeground(Color.BLACK);
-						desb_dragon=false;
-					}
-				}
-			});
-			btnDesbloquearDragon.setBounds(229, 168, 150, 25);
-			contentPanel.add(btnDesbloquearDragon);
-		}
-		{
-			JLabel lblCreatedByYago = new JLabel("Created by Yago Echave-Sustaeta");
-			lblCreatedByYago.setForeground(Color.BLACK);
-			lblCreatedByYago.setFont(new Font("Tahoma", Font.BOLD, 13));
-			lblCreatedByYago.setBounds(12, 210, 258, 16);
-			contentPanel.add(lblCreatedByYago);
-		}
-		JButton okButton = new JButton("OK");
+		
+		//------------------------------------Buttons------------------------------------------//
+		
+		BT_unlock_wolf = new JButton("Desbloquear Lobo");
+		BT_unlock_wolf.setForeground(Color.BLACK);
+		BT_unlock_wolf.setBounds(35, 168, 150, 25);
+		contentPanel.add(BT_unlock_wolf);
+		
+		BT_unlock_dragon = new JButton("Desbloquear Drag\u00F3n");
+		BT_unlock_dragon.setForeground(Color.BLACK);
+		BT_unlock_dragon.setBounds(229, 168, 150, 25);
+		contentPanel.add(BT_unlock_dragon);
+		
+		okButton = new JButton("OK");
 		okButton.setBounds(287, 206, 55, 25);
 		contentPanel.add(okButton);
+		
+		cancelButton = new JButton("Cancel");
+		cancelButton.setActionCommand("Cancel");
+		cancelButton.setBounds(349, 206, 75, 25);
+		contentPanel.add(cancelButton);
+		
+		//------------------------------------Labels------------------------------------------//
+		
+		JLabel lblCreatedByYago = new JLabel("Created by Yago Echave-Sustaeta");
+		lblCreatedByYago.setForeground(Color.BLACK);
+		lblCreatedByYago.setBounds(12, 210, 258, 16);
+		lblCreatedByYago.setFont(new Font("Tahoma", Font.BOLD, 13));
+		contentPanel.add(lblCreatedByYago);
+		
+		JLabel lblNewLabel_1 = new JLabel("Versión 1.3.5");
+		lblNewLabel_1.setBounds(12, 225, 120, 14);
+		contentPanel.add(lblNewLabel_1);
+
+		JLabel lblNewLabel = new JLabel();
+		lblNewLabel.setBounds(0, 0, 450, 253);
+		lblNewLabel.setIcon(new ImageIcon(Info_VT.class.getResource("/Img/background.jpg")));
+		contentPanel.add(lblNewLabel);
+		
+		
+		//------------------------------------Initialize Variables--------------------------------//
+		listeners();
+		initialize();
+	}
+	
+	public void listeners() {
+		
+		this.addWindowListener(new WindowAdapter() {
+	        public void windowClosing(WindowEvent event) {
+	            hideIt();
+	        }
+	    });
+		
+		BT_unlock_wolf.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if(!unlock_wolf) {
+					BT_unlock_wolf.setForeground(Color.RED);
+					unlock_wolf=true;
+				}
+				else{
+					BT_unlock_wolf.setForeground(Color.BLACK);
+					unlock_wolf=false;
+				}
+			}
+		});
+		
+		BT_unlock_dragon.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(!unlock_dragon) {
+					BT_unlock_dragon.setForeground(Color.RED);
+					unlock_dragon=true;
+				}
+				else {
+					BT_unlock_dragon.setForeground(Color.BLACK);
+					unlock_dragon=false;
+				}
+			}
+		});
+		
 		okButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				boolean send=false;
-				if(desb_lobo && desb_dragon) {
+				if(unlock_wolf && unlock_dragon) {
 					JOptionPane.showMessageDialog(null,"Lobo y Dragón desbloqueados correctamente");
 					send = true;
 				}
-				if(desb_lobo) {
+				if(unlock_wolf) {
 					panel.send("soltar_lobo,"+System.getProperty("user.name"));
-					btnDesbloquearLobo.setForeground(Color.BLACK);
-					desb_lobo=false;
+					BT_unlock_wolf.setForeground(Color.BLACK);
+					unlock_wolf=false;
 					if(!send)JOptionPane.showMessageDialog(null,"Lobo desbloqueado correctamente");
 				}
-				if(desb_dragon){
+				if(unlock_dragon){
 					panel.send("soltar_dragon,"+System.getProperty("user.name"));
-					btnDesbloquearDragon.setForeground(Color.BLACK);
-					desb_dragon=false;
+					BT_unlock_dragon.setForeground(Color.BLACK);
+					unlock_dragon=false;
 					if(!send)JOptionPane.showMessageDialog(null,"Dragón desbloqueado correctamente");
 				}
 				hideIt();
 			}
 		});
-		okButton.setActionCommand("OK");
-		getRootPane().setDefaultButton(okButton);
-		{
-			JButton cancelButton = new JButton("Cancel");
-			cancelButton.setBounds(349, 206, 75, 25);
-			contentPanel.add(cancelButton);
-			cancelButton.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent e) {
-					hideIt();
-				}
-			});
-			cancelButton.setActionCommand("Cancel");
-		}
 		
-		JLabel lblNewLabel_1 = new JLabel("Version 1.3.5");
-		lblNewLabel_1.setBounds(12, 225, 120, 14);
-		contentPanel.add(lblNewLabel_1);
-		{
-			JLabel lblNewLabel = new JLabel("New label");
-			lblNewLabel.setBounds(0, 0, 450, 253);
-			contentPanel.add(lblNewLabel);
-			lblNewLabel.setIcon(new ImageIcon(Info_VT.class.getResource("/Img/background.jpg")));
-		}
+		cancelButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				hideIt();
+			}
+		});
 	}
 	
-	public void showIt(boolean lobo, boolean dragon) {
+	
+	public void initialize() {
+		unlock_dragon=false;
+		unlock_wolf=false;
+	}
+	
+	
+	public void showIt(boolean wolf, boolean dragon) {
 		this.setVisible(true);
-		if(!lobo) {
-			btnDesbloquearLobo.setEnabled(false);
+		if(!wolf) {
+			BT_unlock_wolf.setEnabled(false);
 		}
 		if(!dragon){
-			btnDesbloquearDragon.setEnabled(false);
+			BT_unlock_dragon.setEnabled(false);
 		}
 		
 	}
 	
 	public void hideIt() {
-		btnDesbloquearLobo.setForeground(Color.BLACK);
-		btnDesbloquearLobo.setEnabled(true);
-		desb_lobo=false;
-		btnDesbloquearDragon.setForeground(Color.BLACK);
-		btnDesbloquearDragon.setEnabled(true);
-		desb_dragon=false;
+		
 		Info_VT.this.setVisible(false);
+		
+		unlock_wolf=false;
+		unlock_dragon=false;
+		BT_unlock_wolf.setEnabled(true);
+		BT_unlock_dragon.setEnabled(true);
+		BT_unlock_wolf.setForeground(Color.BLACK);
+		BT_unlock_dragon.setForeground(Color.BLACK);
 	}
 }
