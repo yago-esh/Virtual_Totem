9b38369efa92fa79284c1ca384e2606470e7a473
yago.esh <yago.esh@gmail.com>
2018-04-18 12:41:28 +0200

    Minimize option finished
    minimize option finished

--- src/Virtual_Totem/Read_Data.java
+++ src/Virtual_Totem/Read_Data.java
@@ -2,8 +2,10 @@ package Virtual_Totem;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.PrintWriter;
 
 public class Read_Data {
@@ -13,24 +15,37 @@ public class Read_Data {
 	private BufferedReader br = null;
 	private FileWriter fichero = null;
 	private PrintWriter pw = null;
+	private int size;
 	
-	public Read_Data() {
-		
+	public Read_Data(Integer size) {
+		this.size=size;
 	}
 	
-	public void read() {
+	public String[] read() {
+		
+		String[] data = new String[size];
 		try {
-	         archivo = new File ("C:\\Users\\"+System.getProperty("user.name")+"\\Documents\\Virtual_Totem\\prueba.txt");
-	         fr = new FileReader (archivo);
-	         br = new BufferedReader(fr);
+	        archivo = new File ("C:\\Users\\"+System.getProperty("user.name")+"\\Documents\\Virtual_Totem\\Data_Options.txt");
+	        fr = new FileReader (archivo);
+	        br = new BufferedReader(fr);
 
-	         String linea;
-	         while((linea=br.readLine())!=null)
-	            System.out.println(linea);
+	        String linea;
+	        int x=0;
+	        while((linea=br.readLine())!=null) {
+	        	if(x>=size) {
+	        		//Insertar mensaje de error
+	        		System.out.println("Error, demasiados datos");
+	        	}
+	        	data[x]=linea;
+	        	x++;
+	        }
 	      }
-	      catch(Exception e){
-	         e.printStackTrace();
-	      }finally{
+	      catch(FileNotFoundException e){
+	    	  System.out.println("ARCHIVO NO ENCONTRADO");
+	    	  return null;
+	      } catch (IOException e) {
+			e.printStackTrace();
+		}finally{
 	         try{                    
 	            if( null != fr ){   
 	               fr.close();     
@@ -39,17 +54,19 @@ public class Read_Data {
 	            e2.printStackTrace();
 	         }
 	      }
+		return data;
 	}
 	
-	public void write() {
+	public void write(String[] options) {
 		try
         {
 			new File("C:\\Users\\"+System.getProperty("user.name")+"\\Documents\\Virtual_Totem").mkdirs();
-            fichero = new FileWriter("C:\\Users\\"+System.getProperty("user.name")+"\\Documents\\Virtual_Totem\\prueba.txt");
+            fichero = new FileWriter("C:\\Users\\"+System.getProperty("user.name")+"\\Documents\\Virtual_Totem\\Data_Option.txt");
             pw = new PrintWriter(fichero);
             
-            for (int i = 0; i < 10; i++)
-                pw.println("Linea " + i);
+            for (int x=0; x<options.length; x++) {
+                pw.println(options[x]);
+            }
 
         } catch (Exception e) {
             e.printStackTrace();
--- src/Virtual_Totem/Window_VT.java
+++ src/Virtual_Totem/Window_VT.java
@@ -27,13 +27,17 @@ import Virtual_Totem.Window_VT;
 class Window_VT extends JFrame {
 
 	private Client_VT cliente;
+	private Read_Data IO;
 	private Panel_VT Panel_VT;
 	private MenuItem exitItem, showItem;
 	private TrayIcon trayIcon;
 	private String[] options;
+	private CheckboxMenuItem OpcMinimize;
+	private static int num_options = 1; 
 	static final long serialVersionUID = 42L;
 
 	public Window_VT(Client_VT cliente) {
+		
 		this.cliente = cliente;
 		Panel_VT = new Panel_VT(cliente);
 		this.setContentPane(Panel_VT);
@@ -43,56 +47,52 @@ class Window_VT extends JFrame {
 		this.setIconImage(Toolkit.getDefaultToolkit().getImage(getClass().getResource("/Img/totem.png")));
 		this.setResizable(false);
 		this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		
 		Systray();
 		listeners();
-		
-		new Read_Data().read();
+		initializer();
 	}
+	
+	public void initializer() {
+		
+		IO = new Read_Data(num_options);
+		options = IO.read();
+		if(options != null) {
+			setoptions();
+		}
+		else {
+			options = new String[num_options];
+		}
+	}
+	
+	public void setoptions() {
+		
+		OpcMinimize.setState(Boolean.valueOf(options[0]));
+		
+	}
+	
+	public void getoptions() {
+		
+		options[0]=String.valueOf(OpcMinimize.getState());
+		
+	}
+	
 	
 	public void listeners() {
 		this.addWindowListener(new WindowAdapter() {
 			public void windowClosing(WindowEvent e) {
-				setVisible(false);
+				if(OpcMinimize.getState()) {
+					setVisible(false);
+				}
+				else {
+					exit();
+				}
 			}
 		});
 		
 		exitItem.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-            	System.out.println(Panel_VT.cant_exit());
-				if(Panel_VT.cant_exit()) {
-					String ObjButtons[] = {"Yes","No"};
-			        int PromptResult = JOptionPane.showOptionDialog(null,"¿Estás seguro de que quieres salir? Tienes un totem cogido.",
-			        		"Advertencia de salida",JOptionPane.DEFAULT_OPTION,JOptionPane.WARNING_MESSAGE,null,ObjButtons,ObjButtons[1]);
-			        if(PromptResult==JOptionPane.YES_OPTION)
-			        {
-			        	Panel_VT.getAlert_VT().removeMeUserFromList();
-			        	
-			        	if (Panel_VT.warning_wolf()) {
-			        		Panel_VT.blockWolf("");
-			    			cliente.enviar("soltar_lobo");
-			    		}
-			    		if ( Panel_VT.warning_dragon()) {
-			    			Panel_VT.blockDragon("");
-			    			cliente.enviar("soltar_dragon");
-			    		}
-			    		
-			        	try {
-			        		//Need to wait 2 second to let enough time to the server to read the last message and be available to read the next one before close the flows.
-							Thread.sleep(1000);
-						} catch (InterruptedException e1) {
-							// TODO Auto-generated catch block
-							e1.printStackTrace();
-						}
-			        	
-			        	Window_VT.this.cliente.terminar();
-			            System.exit(-1);
-			        }
-				}
-				else {
-					Panel_VT.getAlert_VT().removeMeUserFromList();
-					Window_VT.this.cliente.terminar();
-		            System.exit(-1);
-				}
+				exit();
             }
         });
 		
@@ -126,7 +126,7 @@ class Window_VT extends JFrame {
 	        exitItem = new MenuItem("Salir");
 	        showItem = new MenuItem("Abrir");
 	        Menu options = new Menu("Opciones");
-	        CheckboxMenuItem OpcMinimize = new CheckboxMenuItem("Minimizar al cerrar");
+	        OpcMinimize = new CheckboxMenuItem("Minimizar al cerrar");
 	       
 	        //Add components to pop-up menu
 	        popup.add(showItem);
@@ -148,6 +148,46 @@ class Window_VT extends JFrame {
 	
 	public void showIt() {
 		this.setVisible(true);
+	}
+	
+	public void exit() {
+		if(Panel_VT.cant_exit()) {
+			String ObjButtons[] = {"Yes","No"};
+	        int PromptResult = JOptionPane.showOptionDialog(null,"¿Estás seguro de que quieres salir? Tienes un totem cogido.",
+	        		"Advertencia de salida",JOptionPane.DEFAULT_OPTION,JOptionPane.WARNING_MESSAGE,null,ObjButtons,ObjButtons[1]);
+	        if(PromptResult==JOptionPane.YES_OPTION)
+	        {
+	        	getoptions();
+	        	IO.write(options);
+	        	Panel_VT.getAlert_VT().removeMeUserFromList();
+	        	
+	        	if (Panel_VT.warning_wolf()) {
+	        		Panel_VT.blockWolf("");
+	    			cliente.enviar("soltar_lobo");
+	    		}
+	    		if ( Panel_VT.warning_dragon()) {
+	    			Panel_VT.blockDragon("");
+	    			cliente.enviar("soltar_dragon");
+	    		}
+	    		
+	        	try {
+	        		//Need to wait 2 second to let enough time to the server to read the last message and be available to read the next one before close the flows.
+					Thread.sleep(1000);
+				} catch (InterruptedException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+	        	Window_VT.this.cliente.terminar();
+	            System.exit(-1);
+	        }
+		}
+		else {
+			getoptions();
+        	IO.write(options);
+			Panel_VT.getAlert_VT().removeMeUserFromList();
+			Window_VT.this.cliente.terminar();
+            System.exit(-1);
+		}
 	}
 }
 
